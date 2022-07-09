50a1b6814540c174f0615317fdf70fad01d40029
Abl02 <108988828+Abl02@users.noreply.github.com>
2022-07-09 18:15:44 +0200

    animation to player

--- main.py
+++ main.py
@@ -17,10 +17,33 @@ if __name__ == "__main__":
 
         def __init__(self):
             super().__init__()
-            self.image = pygame.image.load('Red_Ballon_1.png')
-            self.rect = self.image.get_rect()
+
+            self.image1 = pygame.image.load('Red_Balloon_1.png')
+            self.image2 = pygame.image.load('Red_Balloon_2.png')
+            self.image3 = pygame.image.load('Red_Balloon_3.png')
+            self.image4 = pygame.image.load('Red_Balloon_4.png')
+            self.image3 = pygame.image.load('Red_Balloon_3.png')
+            self.image2 = pygame.image.load('Red_Balloon_2.png')
+            self.animation_index = 0
+            self.index = 0
+            self.animation_list = []
+            self.animation_list.append(self.image1)
+            self.animation_list.append(self.image2)
+            self.animation_list.append(self.image3)
+            self.animation_list.append(self.image4)
+            self.animation_list.append(self.image3)
+            self.animation_list.append(self.image2)
+
             self.pos_x = 50
             self.pos_y = 50
+
+        def animation(self):
+            self.rect = self.animation_list[self.index].get_rect()
+            if self.animation_index > 5.9:
+                self.animation_index = 0
+            self.animation_index += 0.08
+            self.index = int(self.animation_index)
+            print(int(self.animation_index))
 
     balloon = Balloon()
 
@@ -30,10 +53,11 @@ if __name__ == "__main__":
 
         screen.fill((200, 200, 200))
 
-        screen.blit(balloon.image, (Balloon.pos_x, Balloon.pos_y))
-        
-        clock.tick(1000)
+        screen.blit(balloon.animation_list[balloon.index], (balloon.pos_x, balloon.pos_y))
+
+        balloon.animation()
 
         pygame.display.flip()
+        clock.tick(60)
 
     pygame.quit()
