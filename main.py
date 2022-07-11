a59f55d9818387bfea9b2acbdff6342e5ba5a602
avetsu <70340986+avetsu@users.noreply.github.com>
2022-07-11 18:36:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pygame
 if __name__ == "__main__":
     pygame.init()
 
-    pygame.display.set_caption("Simulation")
-    screen = pygame.display.set_mode((960, 540))
+    pygame.display.set_caption("Balloon")
+    screen = pygame.display.set_mode((1355, 865))
     clock = pygame.time.Clock()
 
     def quit_game():
@@ -17,10 +17,70 @@ if __name__ == "__main__":
 
         def __init__(self):
             super().__init__()
-            self.image = pygame.image.load('Red_Ballon_1.png')
-            self.rect = self.image.get_rect()
-            self.pos_x = 50
-            self.pos_y = 50
+            self.image1 = pygame.image.load('Red_Balloon_1.png')
+            self.image2 = pygame.image.load('Red_Balloon_2.png')
+            self.image3 = pygame.image.load('Red_Balloon_3.png')
+            self.image4 = pygame.image.load('Red_Balloon_4.png')
+            self.animation_index = 0
+            self.index = 0
+            self.animation_list = []
+            self.animation_list.append(self.image1)
+            self.animation_list.append(self.image2)
+            self.animation_list.append(self.image3)
+            self.animation_list.append(self.image4)
+            self.animation_list.append(self.image3)
+            self.animation_list.append(self.image2)
+
+            self.pos_x = 40
+            self.pos_y = 120
+            self.key_pressed = False
+            self.key_blocked = []
+            self.key_blocked.append(False)
+            self.key_blocked.append(False)
+            self.key_blocked.append(False)
+            self.key_blocked.append(False)
+
+        def animation(self):
+            if self.animation_index == 10:
+                self.animation_index = 0
+                self.index += 1
+                if self.index == 6:
+                    self.index = 0
+            self.rect = self.animation_list[self.index].get_rect()
+            
+            self.animation_index += 1
+
+        def movement(self):
+            if self.key_blocked[0] == True and pygame.key.get_pressed()[pygame.K_LEFT] == False:
+                self.key_blocked[0] = False
+
+            if self.key_blocked[1] == True and pygame.key.get_pressed()[pygame.K_RIGHT] == False: 
+                self.key_blocked[1] = False
+
+            if self.key_blocked[2] == True and pygame.key.get_pressed()[pygame.K_UP] == False:
+                self.key_blocked[2] = False
+            
+            if self.key_blocked[3] == True and pygame.key.get_pressed()[pygame.K_DOWN] == False:
+                self.key_blocked[3] = False
+            
+            if pygame.key.get_pressed()[pygame.K_LEFT] == True and self.pos_x > 40 and self.key_blocked[0] == False:
+                self.pos_x -= 75
+                self.key_blocked[0] = True
+
+            if pygame.key.get_pressed()[pygame.K_RIGHT] == True and self.pos_x < 1240 and self.key_blocked[1] == False:
+                self.pos_x += 75
+                self.key_blocked[1] = True
+
+            if pygame.key.get_pressed()[pygame.K_UP] == True and self.pos_y > 120 and self.key_blocked[2] == False:
+                self.pos_y -= 103
+                self.key_blocked[2] = True
+                
+            if pygame.key.get_pressed()[pygame.K_DOWN] == True and self.pos_y < 635  and self.key_blocked[3] == False:
+                self.pos_y += 103
+                self.key_blocked[3] = True
+                    
+
+
 
     balloon = Balloon()
 
@@ -30,9 +90,14 @@ if __name__ == "__main__":
 
         screen.fill((200, 200, 200))
 
-        screen.blit(balloon.image, (Balloon.pos_x, Balloon.pos_y))
-        
-        clock.tick(1000)
+
+
+        screen.blit(balloon.animation_list[balloon.index], (balloon.pos_x, balloon.pos_y))
+
+        balloon.animation()
+        balloon.movement()
+
+        clock.tick(45)
 
         pygame.display.flip()
 
